2436042c14f203c6cb9ab8757cba8530dbc2b788
Nathan Kramer <nathan.l.kramer@gmail.com>
2017-05-17 17:16:43 +1200

    Add XML parsing with nokogiri

--- lib/apex_rumble/build.rb
+++ lib/apex_rumble/build.rb
@@ -3,6 +3,7 @@ require "fileutils"
 require "apex_rumble/metadata/apex_class"
 require "apex_rumble/metadata/sobject"
 require "apex_rumble/metadata/custom_field"
+require "apex_rumble/metadata/schema_reader"
 module ApexRumble
 
   DEFAULT_SCHEMA_DIR = './src/objects'
@@ -13,66 +14,21 @@ module ApexRumble
     def initialize(schema_dir, output_dir)
       @schema_dir = schema_dir
       @output_dir = output_dir
-      self.validate
-    end
-
-    def validate
-      if !@schema_dir
-        @schema_dir = DEFAULT_SCHEMA_DIR
-        warn 'No salesforce object metadata directory provided. Assuming ' + @schema_dir
-      end
-
-      unless File.directory?(@schema_dir)
-        raise "Directory #{@schema_dir} is not present"
-      end
-
-      if !@output_dir
-        @output_dir = DEFAULT_OUTPUT_DIR
-        warn 'No output directory provided. Assuming ' + @output_dir
-      end
-    end
-
-    def records #@TODO: Parse from the schema_dir argument (assuming *.object XML metadata for now)
-      [
-        ApexRumble::SObject.new('Exception__c', [
-          CustomField.new('Id', 'Id', true),
-          CustomField.new('Name', 'String', true),
-          CustomField.new('Account__c', 'Id', true),
-          CustomField.new('Context__c', 'String', true),
-          CustomField.new('Label__c', 'String', true),
-          CustomField.new('MostRecentOccurrenceDate__c', 'DateTime', false),
-          CustomField.new('Notes__c', 'String', true),
-          CustomField.new('Occurrences__c', 'Decimal', false),
-          CustomField.new('UniqueName__c', 'String', true)
-        ]),
-        ApexRumble::SObject.new('Occurrence__c', [
-          CustomField.new('Id', 'Id', true),
-          CustomField.new('Name', 'String', true),
-          CustomField.new('Cause__c', 'String', true),
-          CustomField.new('Exception__c', 'Id', true),
-          CustomField.new('JobId__c', 'String', true),
-          CustomField.new('Message__c', 'String', true),
-          CustomField.new('OccurrenceDate__c', 'DateTime', true),
-          CustomField.new('OrganizationId__c', 'String', true),
-          CustomField.new('Sandbox__c', 'Boolean', true),
-          CustomField.new('Stacktrace__c', 'String', true),
-          CustomField.new('UserId__c', 'String', true)
-        ])
-      ]
+      validate
     end
 
     def run
       # hard code some sobjects for. In the future these should be worked out in an intelligent fashion
       apex_classes = []
       FileUtils::mkdir_p "#{@output_dir}/classes"
-      staticClasses = Dir.glob("#{ApexRumble.source_dir}/api/*.cls")
-      staticClasses.each { |file_name|
+      static_classes = Dir.glob("#{ApexRumble.source_dir}/api/*.cls")
+      static_classes.each { |file_name|
         content = IO.read(file_name)
         class_name = File.basename(file_name, '.cls')
         apex_classes.push(ApexClass.new(class_name, content))
       }
-      dynamicClasses = Dir.glob("#{ApexRumble.source_dir}/api/*.cls.erb")
-      dynamicClasses.each { |file_name|
+      dynamic_classes = Dir.glob("#{ApexRumble.source_dir}/api/*.cls.erb")
+      dynamic_classes.each { |file_name|
         template = IO.read(file_name)
         class_name = File.basename(file_name, '.cls.erb')
 
@@ -86,7 +42,7 @@ module ApexRumble
         model: IO.read("#{ApexRumble.source_dir}/models/model.cls.erb")
       }
 
-      self.records.each { |record|
+      records.each { |record|
         compiler = ERB.new(templates[:model_base])
         result = compiler.result(binding)
         apex_classes.push(ApexClass.new("Rumble#{record.class_name}", result))
@@ -104,6 +60,29 @@ module ApexRumble
       compiler = ERB.new(package_xml_template)
       result = compiler.result(binding)
       File.write("#{@output_dir}/package.xml", result)
+    end
+
+    private
+
+    def records #@TODO: Parse from the schema_dir argument (assuming *.object XML metadata for now)
+      schema_reader = SchemaReader.new(@schema_dir)
+      schema_reader.records
+    end
+
+    def validate
+      if !@schema_dir
+        @schema_dir = DEFAULT_SCHEMA_DIR
+        warn 'No salesforce object metadata directory provided. Assuming ' + @schema_dir
+      end
+
+      unless File.directory?(@schema_dir)
+        raise "Directory #{@schema_dir} is not present"
+      end
+
+      if !@output_dir
+        @output_dir = DEFAULT_OUTPUT_DIR
+        warn 'No output directory provided. Assuming ' + @output_dir
+      end
     end
 
   end
